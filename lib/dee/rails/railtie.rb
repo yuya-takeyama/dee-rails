e29ebccc2de7684fdbad8589b7272c3cda4c0e7f
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2013-12-31 19:03:44 +0900

    Load generator

--- lib/dee/rails/railtie.rb
+++ lib/dee/rails/railtie.rb
@@ -4,6 +4,10 @@ module Dee
       config.to_prepare do
         ::Dee::Rails.setup!
       end
+
+      generators do
+        require 'dee/rails/generators/service_provider_generator'
+      end
     end
   end
 end
